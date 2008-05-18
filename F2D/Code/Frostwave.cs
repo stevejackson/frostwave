ea495ded58719b2a7de351c218f26c9cd0fbed84
steven.j.jackson <steven.j.jackson@e1adb6f1-c849-0410-9b25-49477284f1fd>
2008-05-18 23:36:14 +0000

    Done with initial implementation of F2D.Frostwave, as well as the accompanying unit tests.  Added SkatePalace for scratch testing.

--- F2D/Code/Frostwave.cs
+++ F2D/Code/Frostwave.cs
@@ -2,27 +2,203 @@
  * (c) Snowfall Media 2008
  * Steven Jackson, Vedran Budimcic
  */
+
 using Microsoft.Xna.Framework;
+using F2D.Math;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace F2D
 {
     /// <summary>
-    /// Window management.
+    /// Window management: interacts with the GraphicsDeviceManager to automatically
+    /// deal with aspect ratios, etc.
     /// </summary>
-    public class Frostwave
+    /// <example>
+    /// Frostwave.Initialize(graphics_device_manager);
+    /// Frostwave.Resolution = new Vector2Int(1024, 768);
+    /// Frostwave.Fullscreen = true;
+    /// Frostwave.CreateDisplay();
+    /// Frostwave.Draw();
+    /// </example>
+    static public class Frostwave
     {
-        private GraphicsDeviceManager graphicsManager;
-        public GraphicsDeviceManager GraphicsManager
+
+        #region Properties        
+        
+        static private GraphicsDeviceManager graphicsManager;
+        static public GraphicsDeviceManager GraphicsManager
         {
             get { return graphicsManager; }
             set { graphicsManager = value; }
         }
 
-        public Frostwave(GraphicsDeviceManager graphicsManager)
+        static private bool fullscreen;
+        static public bool Fullscreen
         {
-            this.graphicsManager = graphicsManager;
+            get { return fullscreen; }
+            set
+            {
+                fullscreen = value;
+                GraphicsManager.IsFullScreen = fullscreen;
+            }
         }
 
+        static private Vector2Int resolution;
+        static public Vector2Int Resolution
+        {
+            get { return resolution; }
+            set
+            {
+                resolution = value;
+
+                GraphicsManager.PreferredBackBufferHeight = resolution.Y;
+                GraphicsManager.PreferredBackBufferWidth = resolution.X;
+
+                Recalculate();
+            }
+        }
+
+        static private int columnBoxSize;
+        static public int ColumnBoxSize
+        {
+            get { return columnBoxSize; }
+            set { columnBoxSize = value; }
+        }
+
+        static private Vector2Int baseResolution;
+        
+        /// <summary>
+        /// The base resolution is what everything will be scaled from upon rendering.
+        /// </summary>
+        static public Vector2Int BaseResolution
+        {
+            get { return baseResolution; }
+            set 
+            { 
+                baseResolution = value;
+                Recalculate();
+            }
+        }
+
+        static private Viewport clearViewport;
+
+        /// <summary>
+        /// This is the viewport rendered in the back - the black bars.
+        /// It's the full size of the screen.
+        /// </summary>
+        static public Viewport ClearViewport
+        {
+            get { return clearViewport; }
+        }
+
+        static private Viewport sceneViewport;
+
+        /// <summary>
+        /// This is the viewport representing the area where the game is rendered.
+        /// </summary>
+        static public Viewport SceneViewport
+        {
+            get { return sceneViewport; }
+        }
+
+
+        static private Vector2 scale;
+
+        /// <summary>
+        /// Represents the scale for all objects to be multiplied by when drawn.
+        /// The ratio is (Resolution / BaseResolution)
+        /// </summary>
+        static public Vector2 Scale
+        {
+            get { return scale; }
+        }
+
+        static private Matrix scaleMatrix;
+
+        /// <summary>
+        /// Another representation of the scale between Resolution & BaseResolution,
+        /// in a Matrix format, for use by SpriteBatch rendering.
+        /// </summary>
+        static public Matrix ScaleMatrix
+        {
+            get { return scaleMatrix; }
+        }
+
+        #endregion
+
+        static public void Initialize(GraphicsDeviceManager graphicsMan)
+        {
+            graphicsManager = graphicsMan;
+
+            // graphics display properties
+            fullscreen = false;
+            resolution = new Vector2Int(1024, 768);
+            baseResolution = new Vector2Int(1600, 1200);
+
+            // set default values for columnbox + viewports
+            sceneViewport = new Viewport();
+            clearViewport = new Viewport();
+
+            Recalculate();
+        }
+
+        /// <summary>
+        /// Updates the GraphicsManager with the current properties.
+        /// </summary>
+        static public void CreateDisplay()
+        {
+            try
+            {
+                GraphicsManager.ApplyChanges();
+            }
+            catch (System.ObjectDisposedException)
+            {
+                //This is an issue w/ NUnit or XNA, just catch & ignore it
+            }
+        }
+
+        /// <summary>
+        /// This should be called every frame before anything else.
+        /// It draws the viewports according to resolution, aspect ratio, etc.
+        /// </summary>
+        static public void Draw()
+        {
+            GraphicsManager.GraphicsDevice.Viewport = ClearViewport;
+            GraphicsManager.GraphicsDevice.Clear(Color.Black);
+            GraphicsManager.GraphicsDevice.Viewport = SceneViewport;
+            GraphicsManager.GraphicsDevice.Clear(Color.CornflowerBlue);
+        }
+
+        /// <summary>
+        /// Calculates the proper columnbox size for the current resolution, and applies it
+        /// to the viewports.
+        /// </summary>
+        private static void Recalculate()
+        {
+            // 1280x1024 is a special case, but should still have no columnboxing
+            if (Resolution == new Vector2Int(1280, 1024))
+                ColumnBoxSize = (int)((Resolution.X - (Resolution.Y * 1.25f)) / 2);
+            else
+                ColumnBoxSize = (int)((Resolution.X - (Resolution.Y * 1.33333333f)) / 2);
+
+            clearViewport.X = 0;
+            clearViewport.Y = 0;
+            clearViewport.Width = Resolution.X;
+            clearViewport.Height = Resolution.Y;
+
+            sceneViewport.X = ColumnBoxSize;
+            sceneViewport.Y = 0;
+            sceneViewport.Width = Resolution.X - (ColumnBoxSize * 2);
+            sceneViewport.Height = Resolution.Y;
+
+            scale = new Vector2((float)Resolution.X / (float)BaseResolution.X, 
+                                (float)Resolution.Y / (float)BaseResolution.Y);
+
+            scaleMatrix = Matrix.CreateScale(
+                (float)Resolution.X / (float)BaseResolution.X,
+                (float)Resolution.Y / (float)BaseResolution.Y,
+                1f);
+        }
     }
 
 }
--- F2DUnitTests/Code/FrostwaveTests.cs
+++ F2DUnitTests/Code/FrostwaveTests.cs
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework.Content;
 using System;
 using Microsoft.Xna.Framework;
 using F2D;
+using F2D.Math;
 
 namespace F2DUnitTests
 {
@@ -38,27 +39,166 @@ namespace F2DUnitTests
         }
 
         /// <summary>
-        /// Ensure the Frostwave instance is created.
-        /// </summary>
-        [Test]
-        public void TestInstantiation()
-        {
-            Frostwave fw = new Frostwave(GraphicsManager);
-            Assert.IsNotNull(fw);
-            fw = null;
-        }
-
-        /// <summary>
         /// Ensure the GraphicsManager is not null.
         /// </summary>
         [Test]
         public void TestGraphicsManager()
         {
-            Frostwave fw = new Frostwave(GraphicsManager);
-            Assert.IsNotNull(fw.GraphicsManager);
-            fw = null;
+            Frostwave.Initialize(GraphicsManager);
+            Assert.IsNotNull(Frostwave.GraphicsManager);
+        }
+
+        /// <summary>
+        /// Ensure the fullscreen property works.
+        /// </summary>
+        [Test]
+        public void TestFullScreenProperty()
+        {
+            Frostwave.Initialize(GraphicsManager);
+            Frostwave.Fullscreen = true;
+
+            Assert.AreEqual(true, Frostwave.Fullscreen);
+            Assert.AreEqual(true, Frostwave.GraphicsManager.IsFullScreen); 
+        }
+
+        /// <summary>
+        /// Ensures the Frostwave resolution property works.
+        /// </summary>
+        [Test]
+        public void TestResolutionProperty()
+        {
+            Frostwave.Initialize(GraphicsManager);
+            Frostwave.Resolution = new Vector2Int(800, 600);
+
+            Vector2Int expected = new Vector2Int(800, 600);
+
+            Assert.AreEqual(expected, Frostwave.Resolution);
+            Assert.AreEqual(expected.X, Frostwave.GraphicsManager.PreferredBackBufferWidth);
+            Assert.AreEqual(expected.Y, Frostwave.GraphicsManager.PreferredBackBufferHeight);
         }
 
 
+        /// <summary>
+        /// Ensure the correct columnboxing value is calculated for 1024x768.
+        /// </summary>
+        [Test]
+        public void TestColumnboxingValue_1024x768()
+        {
+            Frostwave.Initialize(GraphicsManager);
+            Frostwave.Resolution = new Vector2Int(1024, 768);
+            Assert.AreEqual(0, Frostwave.ColumnBoxSize);
+        }
+
+        /// <summary>
+        /// Ensure the correct columnboxing value is calculated for 1280x1024.
+        /// </summary>
+        [Test]
+        public void TestColumnboxingValue_1280x1024()
+        {
+            Console.WriteLine(Frostwave.Resolution);
+            Frostwave.Initialize(GraphicsManager);
+            Frostwave.Resolution = new Vector2Int(1280, 1024);
+            Assert.AreEqual(0, Frostwave.ColumnBoxSize);
+        }
+
+        /// <summary>
+        /// Ensure the correct columnboxing value is calculated for 1680x1050.
+        /// </summary>
+        [Test]
+        public void TestColumnboxingValue_1680x1050()
+        {
+            Frostwave.Initialize(GraphicsManager);
+            Frostwave.Resolution = new Vector2Int(1680, 1050);
+            Assert.AreEqual(140f, Frostwave.ColumnBoxSize, 2.0f);
+        }
+
+        /// <summary>
+        /// Ensure the viewport values are set properly for 1024x768.
+        /// </summary>
+        [Test]
+        public void TestViewportValues_1024x768()
+        {
+            Frostwave.Initialize(GraphicsManager);
+            Frostwave.Resolution = new Vector2Int(1024, 768);
+
+            Assert.AreEqual(1024, Frostwave.ClearViewport.Width);
+            Assert.AreEqual(1024, Frostwave.SceneViewport.Width);
+            Assert.AreEqual(768, Frostwave.SceneViewport.Height);
+            Assert.AreEqual(0, Frostwave.SceneViewport.X);
+        }
+
+        /// <summary>
+        /// Ensure the viewport values are set properly for 1280x1024.
+        /// </summary>
+        [Test]
+        public void TestViewportValues_1280x1024()
+        {
+            Frostwave.Initialize(GraphicsManager);
+            Frostwave.Resolution = new Vector2Int(1280, 1024);
+
+            Assert.AreEqual(1280, Frostwave.ClearViewport.Width);
+            Assert.AreEqual(1280, Frostwave.SceneViewport.Width);
+            Assert.AreEqual(1024, Frostwave.SceneViewport.Height);
+            Assert.AreEqual(0, Frostwave.SceneViewport.X);
+        }
+
+        /// <summary>
+        /// Ensure the viewport values are set properly for 1680x1050.
+        /// </summary>
+        [Test]
+        public void TestViewportValues_1680x1050()
+        {
+            Frostwave.Initialize(GraphicsManager);
+            Frostwave.Resolution = new Vector2Int(1680, 1050);
+
+            Assert.AreEqual(1680, Frostwave.ClearViewport.Width);
+            Assert.AreEqual(1402, Frostwave.SceneViewport.Width);
+            Assert.AreEqual(1050, Frostwave.SceneViewport.Height);
+            Assert.AreEqual(139, Frostwave.SceneViewport.X);
+        }
+
+        /// <summary>
+        /// Ensure the base resolution property works.
+        /// </summary>
+        [Test]
+        public void TestBaseResolutionProperty()
+        {
+            Frostwave.Initialize(GraphicsManager);
+            Frostwave.BaseResolution = new Vector2Int(1024, 768);
+
+            Assert.AreEqual(new Vector2Int(1024, 768), Frostwave.BaseResolution);
+        }
+
+        /// <summary>
+        /// Ensure the scale property is calculated correctly.
+        /// </summary>
+        [Test]
+        public void TestScaleValue_1024x768()
+        {
+            Frostwave.Initialize(GraphicsManager);
+            
+            Frostwave.Resolution = new Vector2Int(1024, 768);
+            Frostwave.BaseResolution = new Vector2Int(1600, 1200);
+
+            Vector2 expected = new Vector2(1024f / 1600f, 768f / 1200f);
+            
+            Assert.AreEqual(expected, Frostwave.Scale);
+        }
+        
+        /// <summary>
+        /// Ensure the scale matrix is calculated correctly.
+        /// </summary>
+        [Test]
+        public void TestScaleMatrixValue_1024x768()
+        {
+            Frostwave.Initialize(GraphicsManager);
+
+            Frostwave.Resolution = new Vector2Int(1024, 768);
+            Frostwave.BaseResolution = new Vector2Int(1600, 1200);
+
+            Matrix expected = Matrix.CreateScale((float)(1024f / 1600f), (float)(768f / 1200f), 1f);
+
+            Assert.AreEqual(expected, Frostwave.ScaleMatrix);
+        }
     }
 }
