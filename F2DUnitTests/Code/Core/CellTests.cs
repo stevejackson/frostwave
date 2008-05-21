d4ae12b0c22b1fbb6686bcdee0319a8ad03f9a4d
vedranb@gmail.com <vedranb@gmail.com@e1adb6f1-c849-0410-9b25-49477284f1fd>
2008-05-21 03:58:08 +0000

    Added Cell

---
+++ F2DUnitTests/Code/Core/CellTests.cs
@@ -0,0 +1,74 @@
+/* Frostwave 2D
+ * (c) Snowfall Media 2008
+ * Steven Jackson, Vedran Budimcic
+ */
+
+using NUnit.Framework;
+using Microsoft.Xna.Framework;
+using F2D.Graphics;
+using F2D.Core;
+using F2D;
+
+namespace F2DUnitTests.Core
+{
+    /// <summary>
+    /// Unit Tests for the Cell class.
+    /// </summary>
+    [TestFixture]
+    public class CellTests : TestingGrounds.Game1
+    {
+        /// <summary>
+        /// Ensure the WorldItems property is working.
+        /// </summary>
+        [Test]
+        public void TestWorldItemsProperty()
+        {
+            Cell testCell = new Cell(new Vector2(-300, 200), 50);
+
+            testCell.WorldItems.Add(new WorldImage());
+
+            Assert.IsNotNull(testCell.WorldItems);
+        }
+
+        /// <summary>
+        /// Ensure the Position property is working.
+        /// </summary>
+        [Test]
+        public void TestPositionProperty()
+        {
+            Cell testCell = new Cell(new Vector2(-300, 200), 50);
+
+            testCell.Position = new Vector2(20, 30);
+
+            Assert.AreEqual(new Vector2(20, 30), testCell.Position);
+        }
+
+        /// <summary>
+        /// Ensure the Size property is working.
+        /// </summary>
+        [Test]
+        public void TestSizeProperty()
+        {
+            Cell testCell = new Cell(new Vector2(-300, 200), 50);
+
+            testCell.Size = 90;
+
+            Assert.AreEqual(90, testCell.Size);
+        }
+        
+        /// <summary>
+        /// Ensure the constructor is assigning proper values to variables.
+        /// </summary>
+        [Test]
+        public void TestConstructor()
+        {
+            Cell testCell = new Cell(new Vector2(200, 300), 75);
+
+            Assert.IsNotNull(testCell.WorldItems);
+            Assert.AreEqual(new Vector2(200, 300), testCell.Position);
+            Assert.AreEqual(75, testCell.Size);
+        }
+
+        
+    }
+}
